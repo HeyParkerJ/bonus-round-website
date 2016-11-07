368682412f2efe2c48f42450e999ad398f617c43
Parker Johnson <mrstormthebeach@gmail.com>
2016-11-07 15:40:35 -0700

    Extract font size 1.2 out to font-medium class, move #map styling to .map, update todo

--- js/taphunter.js
+++ js/taphunter.js
@@ -63,9 +63,9 @@ function populatePage(json) {
                     +"<h3 class='card-title'>"+data.title+"</h3>"
                       +"<div class='card-flavor'>"+data.event_start_date+" - "+data.event_start_time+"</div>"
                   +"</div>"
-                  +"<div class='card-content'>"+description+"</div>"
+                  +"<div class='card-content font-medium'>"+description+"</div>"
                 +"</div>"
-              +"<div class='card-footer'><a class='link' href="+data.link+">Read on Facebook</a></div>"
+              +"<div class='card-footer font-medium'><a class='link' href="+data.link+">Read on Facebook</a></div>"
               +"</div>"
     }
   })
